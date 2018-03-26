a938c084d01d8a9ecd1eda4c615f896f23fd6282
cwillia9 <coltonrwilliams@gmail.com>
2018-03-25 19:31:46 -0600

    more data

--- data/scraper/selenium_scrape_seeking_alpha.py
+++ data/scraper/selenium_scrape_seeking_alpha.py
@@ -3,6 +3,8 @@ import csv
 import requests
 import time
 from lxml import html
+from google_search import GoogleDriver
+import sys
 
 def clean_xpath(r):
     return clean_str(r[0].text_content())
@@ -13,7 +15,7 @@ def clean_str(s):
 def get_xpath(xp, tree):
     return clean_xpath(tree.xpath(xp))
 
-def parse_page(url):
+def parse_page(url, time_delay=20):
     driver = None
     try:
         driver = webdriver.Firefox()
@@ -26,10 +28,12 @@ def parse_page(url):
         body = clean_str(body.text)
 
         driver.close()
+        time.sleep(20)                
         return (title, date, body)
     except:
         if driver:
             driver.close()
+            time.sleep(20)                
         return None
 
 def read_all_links(fn, col=0):
@@ -41,17 +45,64 @@ def read_all_links(fn, col=0):
             urls.append(row[fieldname])
     return urls
 
+def eprint(*args):
+    print(*args, file=sys.stderr)
+
+def url_to_initial_state(url):
+    return (url,0,None)
+    
+def attempt_page_scrape(url, delay=20):
+    try:
+        title, date, body = parse_page(url,delay)
+        return (title, date, body)
+    except Exception as e:
+        eprint("Failed for url: {} with reason: {}".format(url, e))
+        return None
+
+
+def attempt_with_state(stateful_url, delay=20):
+    url, attempts, result = stateful_url
+    if result == None:
+        result = attempt_page_scrape(url, delay)
+    return (url, attempts + 1, result)
+    
+
 if __name__ == '__main__':
-    urls = read_all_links('links/all_ibm_seekingalpha_links.csv')
-    with open('seeking_alpha_page_data.csv', 'w') as csvfile:
+    query = sys.argv[1]
+    date_ranges = [('1/1/2013', '6/1/2013'),
+                   ('6/2/2013', '1/1/2014'),
+                   ('1/2/2014', '6/1/2014'),
+                   ('6/2/2014', '1/1/2015'),
+                   ('1/2/2015', '6/1/2015'),
+                   ('6/2/2015', '1/1/2016'),
+                   ('1/2/2016', '6/1/2016'),
+                   ('6/2/2016', '1/1/2017'),
+                   ('1/2/2017', '6/1/2017'),
+                   ('6/2/2017', '1/1/2018'),
+                   ('1/2/2018', '6/1/2018')]
+
+    urls = []
+    for frm, to in date_ranges:
+        try:
+            gdriver = GoogleDriver(query, 2)
+            gdriver.filter_date_range(frm, to)
+            for page in gdriver:
+                urls += gdriver.page_urls()
+            gdriver.close()
+        except:
+            eprint("Failed for date range ({}, {})".format(frm,to))
+
+    stateful_urls = map(url_to_initial_state, urls)
+    for i in range(3):
+        delay = 20 * (i+1)
+        stateful_urls = map(lambda t: attempt_with_state(t, delay), stateful_urls)
+
+    with sys.stdout as csvfile:
         fieldnames = ['title', 'date', 'body']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for url in urls:
-            try:
-                title, date, body = parse_page(url)
-                print("title: {}, date: {}".format(title, date))
+        for _, _, result in stateful_urls:
+            if result:
+                title, date, body = result
+                #eprint("title: {}, date: {}".format(title, date))
                 writer.writerow({'title': title, 'date': date, 'body': body})
-            except Exception as e:
-                print(e)
-            time.sleep(10)                
